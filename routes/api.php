eb4464ea2d3f6fdef76716028c52d93ddc5ab2b2
Roderick sanchez <rodericksanchezmeza@gmail.com>
2021-11-22 20:16:29 -0500

    cambio

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/digimons', function() {
+Route::post('/digimons', function() {
     return Digimon::where('music', '!=', '')->get();
 });
 
