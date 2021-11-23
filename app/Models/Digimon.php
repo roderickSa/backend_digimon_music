0f46939fabf1666f30877ac10e814c2040841cfe
Roderick sanchez <rodericksanchezmeza@gmail.com>
2021-11-22 20:12:43 -0500

    primeri cambio api

--- app/Models/Digimon.php
+++ app/Models/Digimon.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Digimon extends Model
 {
-    use HasFactory;
+
+    protected $table = 'digimon';
+
+    protected $fillable = [
+        'name',
+        'lavel',
+        'img',
+        'music'
+    ];
+
+    public function getDigimons() {
+        $filtro = '';
+        return DB::select('select * from digimon where music != ?', [$filtro]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Digimon;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/digimons', function() {
+    return Digimon::where('music', '!=', '')->get();
+});
+
+// Route::get('/digimons', function() {
+//     $model_digimon = new Digimon();
+//     return  $model_digimon->getDigimons();
+// });
